7cd96bdfd51c6413e977436a0c4f38d6e8b31c5a
Cristofer-Antecao <cr.antecao@duocuc.cl>
2024-04-27 13:34:17 -0400

    Creación tabla reserva mesa
    Se crea la 1era tabla reserva de mesa , se modifica archivo admin.py para que lea la tabla , no se hacen las migraciones

--- WebGastronomicaApp/appweb/models.py
+++ WebGastronomicaApp/appweb/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class Reserva_Mesa(models.Model):
+    id_reserva = models.IntegerField()
+    nombre = models.CharField(max_length=50 , help_text="Ingrese Nombre")
+    correo = models.EmailField(help_text="Ingrese Correo")
+    fecha = models.DateField(help_text="Ingrese fecha")
+    hora = models.IntegerField(help_text="Ingrese hora")
+    cantidad_comensales = models.IntegerField(help_text="Ingrese cantidad de comensales")
+    
+
+    def __str__(self):
+        return self.id_reserva
