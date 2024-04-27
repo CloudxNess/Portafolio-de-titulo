6259f3ec63c2a48241692c4fb844cae56888487f
Cristofer-Antecao <cr.antecao@duocuc.cl>
2024-04-27 17:16:27 -0400

    Models.py modificación 1
    Se agrega tabla de mesa a archivo models, se hace el migrations correspondiente.
    **Importante revisar campo de hora en caso de cualquier cosa , por que marca la hora pero no de la manera que se espera

--- WebGastronomicaApp/appweb/admin.py
+++ WebGastronomicaApp/appweb/admin.py
@@ -4,3 +4,4 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Reserva_Mesa)
+admin.site.register(Mesa)
--- WebGastronomicaApp/appweb/models.py
+++ WebGastronomicaApp/appweb/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
 # Create your models here
+class Mesa (models.Model):
+    ID_Mesa = models.IntegerField(primary_key=True, help_text="Id de la mesa")
+    Nombre_Mesa = models.CharField(max_length=10, help_text="Identificador de la mesa")
+    Estado_Ocupado = models.BooleanField()
+    Estado_Reservado = models.BooleanField()
+
+    def __str__(self):
+        return self.Nombre_Mesa
+
 
 class Reserva_Mesa(models.Model):
-    id_reserva = models.IntegerField()
+    ID_reserva = models.IntegerField()
     nombre = models.CharField(max_length=50 , help_text="Ingrese Nombre")
     correo = models.EmailField(help_text="Ingrese Correo")
     fecha = models.DateField(help_text="Ingrese fecha")
-    hora = models.IntegerField(help_text="Ingrese hora")
+    hora = models.TimeField(help_text="Ingrese hora")
     cantidad_comensales = models.IntegerField(help_text="Ingrese cantidad de comensales")
-    
+    ID_Mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE, help_text="Id de la mesa")
 
     def __str__(self):
-        return self.id_reserva
+        return self.ID_reserva 
